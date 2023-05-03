87a2e72e3bacb2d5cbddc314f3d49df99090f5c2
Marco Cordero <is727272@iteso.mx>
2023-05-03 13:35:04 -0600

    Github Actions Examen 3

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const moment = require("moment");
+/*
+// Actividad 26-04-2023
+const moment = require("moment"); 
 let i = 5;
 let j = 10;
 console.log('Iniciando...');
 console.log("La suma es " + (5+10));
 
 let fecha = moment().format("LL");
-console.log("Y la fecha es:", fecha);
+console.log("Y la fecha es:", fecha);*/
+
+// Examen 3
+console.log('Nombre: Cordero Hernández, Marco Ricardo');
 
